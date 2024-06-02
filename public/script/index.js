778338fb99123da89b94cd01168a494288876af6
mczionjohnson <mczionjohnson@gmail.com>
2024-06-02 23:53:32 +0100

    set delay and auto-scrolling

--- public/script/index.js
+++ public/script/index.js
@@ -2,23 +2,24 @@ const sendBtn = document.getElementById("sendBtn");
 const textbox = document.getElementById("textbox");
 const chatContainer = document.getElementById("chatContainer");
 
-let user = { message: "" };
+let user = [
+  { greeting: "" },
+  {
+    menu: "",
+  },
+];
 
 let possibleGreetings =
-  "Welcome! How may I serve you today? <br> Please select from the list below: <br>Select 1 to Place an order <br>Select 99 to checkout order <br>Select 98 to see order history <br>Select 97 to see current order <br>Select 0 or exit to cancel order";
+  "Welcome! How may I serve you today? <br> Please select from the list below: <br>Select 1 to Place an order <br>Select 99 to checkout order <br>Select 98 to see order history <br>Select 97 to see current order <br>Select 0 or end chat to cancel order";
+const chatbotError =
+  "Oops! I am a new bot in town and learning with time. I understand hello or end chat";
+let menu =
+  "Please select from the list below: <br>Select 1 for fiesta rice <br>Select 2 for spaghetti <br>Select 3 for fried rice <br>Select 4 for yam and egg sauce <br>Select 0 or end chat to cancel order";
 
-// let arrayOfPossibleMessage = [
-//   {
-//     message: "hello",
-//     response:
-//       "Welcome! How may I serve you today? <br> Please select from the list below <br>Select 1 to Place an order <br>Select 99 to checkout order <br>Select 98 to see order history <br>Select 97 to see current order <br>Select 0 to cancel order",
-//   },
-//   {
-//     message: "Hello",
-//     response:
-//       "Welcome! How may I serve you today? <br> Please select from the list below <br>Select 1 to Place an order <br>Select 99 to checkout order <br>Select 98 to see order history <br>Select 97 to see current order <br>Select 0 to cancel order",
-//   },
-// ];
+const sendChatbotResponse = (userMessageText, chatbotError) => {
+  sendMessage(userMessageText);
+  chatbotBasic(chatbotError);
+};
 
 const sendMessage = (userMessage) => {
   // create a div and input message with + +
@@ -34,47 +35,20 @@ const sendMessage = (userMessage) => {
   chatContainer.appendChild(messageElement);
 };
 
-// greetings
 const chatbotBasic = (chatbotMessage) => {
   const messageElement = document.createElement("div");
 
   messageElement.innerHTML =
     "<span> Chatbot: </span>" + "<span>" + chatbotMessage + "</span>";
 
-  // display it in the container
-  chatContainer.appendChild(messageElement);
+  // delay the response by 1 second to mimic processing
+  setTimeout(() => {
+    // display it in the container
+    chatContainer.appendChild(messageElement);
+    // scroll to the new element
+    chatContainer.lastChild.scrollIntoView({ behavior: 'smooth' });
+  }, 1000);
 };
-
-// const chatbotResponse = (userMessage) => {
-//   let chatbotMessage = "";
-//   if (userMessage == "") {
-//     alert("Please type a message");
-//   }
-//   // search the array for possible response
-//   //   filter each object and regex to lowercase
-//   else if (userMessage.length > 3) {
-//     const result = arrayOfPossibleMessage.filter((val) =>
-//       val.message.includes(userMessage)
-//     );
-//     // alert(userMessage.toLowerCase());
-//     // alert(result[0]);
-//     if (result.length > 0) {
-//       // pick the response in the first object found
-//       const response = result[0].response;
-//       chatbotMessage = response;
-//     } else {
-//       chatbotMessage = "say something else or exit";
-//     }
-//   }
-
-//   const messageElement = document.createElement("div");
-
-//   messageElement.innerHTML =
-//     "<span> Chatbot: </span>" + "<span>" + chatbotMessage + "</span>";
-
-//   // display it in the container
-//   chatContainer.appendChild(messageElement);
-// };
 
 sendBtn.addEventListener("click", (e) => {
   // alert("hello")
@@ -83,9 +57,9 @@ sendBtn.addEventListener("click", (e) => {
 
   if (userMessage == "") {
     alert("Please type a message");
-  } else if (userMessage == "exit") {
+  } else if (userMessage == "end chat") {
     // if user wants to exit
-    let userMessageText = "exit";
+    let userMessageText = userMessage;
     sendMessage(userMessageText);
     alert("Have a nice time");
     textbox.value = "";
@@ -94,9 +68,12 @@ sendBtn.addEventListener("click", (e) => {
     // remove whitespace
     let userMessageText = userMessage.trim();
     // alert(userMessageText)
-    user.message = userMessageText;
+    user.greeting = userMessageText;
     textbox.value = "";
+
+    // re write code here to use dynamic function
     sendMessage(userMessageText);
+    // console.log(user)
 
     let chatbotMessage = possibleGreetings;
     chatbotBasic(chatbotMessage);
@@ -111,12 +88,33 @@ sendBtn.addEventListener("click", (e) => {
 
   //   chatbotResponse(userMessageText);
   // }
-  else {
-    chatbotMessage = "Oops! I am a new bot in town and learning with time. I understand hello or exit";
-    let userMessageText = userMessage.trim();
+  else if (userMessage == 1) {
+    if (user.greeting == undefined) {
+      console.log(user);
+      let userMessageText = userMessage.trim();
 
-    sendMessage(userMessageText);
-    chatbotBasic(chatbotMessage);
+      textbox.value = "";
+
+      sendChatbotResponse(userMessageText, chatbotError);
+    } else {
+      // console.log("here now")
+      let userMessageText = userMessage.trim();
+      user.message = userMessageText;
+      textbox.value = "";
+      let chatbotMenuResponse = menu;
+      // set unique id for scrolling
+
+      sendChatbotResponse(userMessageText, chatbotMenuResponse);
+
+      // document.getElementById('here').scrollIntoView();
+      // messageElement.scrollIntoView();
+      // document.getElementById('here').scrollIntoView();
+      // document.querySelector('#div').scrollIntoView({behavior: 'smooth'});
+    }
+  } else {
+    let userMessageText = userMessage.trim();
     textbox.value = "";
+    sendChatbotResponse(userMessageText, chatbotError);
+    // textbox.value = "";
   }
 });
