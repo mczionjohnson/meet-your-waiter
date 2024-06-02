bb0bebc9781bae8e434b344fe9ad2bf97e113f07
mczionjohnson <mczionjohnson@gmail.com>
2024-06-02 02:33:26 +0100

    fixing response filter

--- public/script/main.js
+++ public/script/main.js
@@ -4,12 +4,9 @@ const chatContainer = document.getElementById("chatContainer");
 
 let user = { message: "" };
 
-
 let arrayOfPossibleMessage = [
-    {message: "Hello", response: "Hi, how can I serve you?"},
-    {message: "",  response: ""},
-    {message: "",  response: ""}
-]
+  { message: "Hello", response: "Hi, how can I serve you?" },
+];
 
 const sendMessage = (userMessage) => {
   // create a div and input message with + +
@@ -26,7 +23,26 @@ const sendMessage = (userMessage) => {
 };
 
 const chatbotResponse = (userMessage) => {
-  let chatbotMessage = "hi";
+  let chatbotMessage = "";
+  if (userMessage == "") {
+    alert("Please type a message");
+  }
+  // search the array for possible response
+  //   filter each object and regex to lowercase
+  else if (userMessage.length > 3) {
+    const result = arrayOfPossibleMessage.filter((val) =>
+      val.message.includes(userMessage)
+    );
+    // alert(userMessage.toLowerCase());
+    // alert(result[0]);
+    if (result.length > 0) {
+        // pick the response in the first object found
+        const response = result[0].response;
+        chatbotMessage = response
+    } else {
+        chatbotMessage = "say something else or exit"
+    }
+  }
 
   const messageElement = document.createElement("div");
 
@@ -51,6 +67,7 @@ sendBtn.addEventListener("click", (e) => {
     user.message = userMessageText;
     textbox.value = "";
     sendMessage(userMessageText);
+
     chatbotResponse(userMessageText);
   }
 });
