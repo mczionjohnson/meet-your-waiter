2c057da6f5c7b34c8a86ba1ddad381277df63d00
mczionjohnson <mczionjohnson@gmail.com>
2024-06-02 01:44:35 +0100

    initial commit

--- public/script/main.js
+++ public/script/main.js
@@ -1,23 +1,56 @@
 const sendBtn = document.getElementById("sendBtn");
 const textbox = document.getElementById("textbox");
+const chatContainer = document.getElementById("chatContainer");
 
-const user = { message: "" };
+let user = { message: "" };
 
-const sendMessage = () => {
+
+let arrayOfPossibleMessage = [
+    {message: "Hello", response: "Hi, how can I serve you?"},
+    {message: "",  response: ""},
+    {message: "",  response: ""}
+]
+
+const sendMessage = (userMessage) => {
+  // create a div and input message with + +
+  // style the element
   const messageElement = document.createElement("div");
+  messageElement.style.textAlign = "right";
+  messageElement.style.margin = "10px";
 
   messageElement.innerHTML =
     "<span> You: </span>" + "<span>" + userMessage + "</span>";
 
+  // display it in the container
   chatContainer.appendChild(messageElement);
 };
 
-sendBtn.addEventListener("click", () => {
+const chatbotResponse = (userMessage) => {
+  let chatbotMessage = "hi";
+
+  const messageElement = document.createElement("div");
+
+  messageElement.innerHTML =
+    "<span> Chatbot: </span>" + "<span>" + chatbotMessage + "</span>";
+
+  // display it in the container
+  chatContainer.appendChild(messageElement);
+};
+
+sendBtn.addEventListener("click", (e) => {
+  // alert("hello")
   const userMessage = textbox.value;
+  // alert(userMessage)
 
   if (userMessage == "") {
+    alert("Please type a message");
   } else {
     // remove whitespace
-    userMessage.trim();
+    let userMessageText = userMessage.trim();
+    // alert(userMessageText)
+    user.message = userMessageText;
+    textbox.value = "";
+    sendMessage(userMessageText);
+    chatbotResponse(userMessageText);
   }
 });
